3095d1731fd36c4f82e3f415bdddc1e722bb8029
Rendall Schijven <rendall.schijven@hotmail.com>
2018-02-11 00:52:11 +0100

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Concurrent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Concurrent.dir/mandelbrot.cpp.o"
+  "Concurrent.pdb"
+  "Concurrent.exe"
+  "libConcurrent.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Concurrent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
