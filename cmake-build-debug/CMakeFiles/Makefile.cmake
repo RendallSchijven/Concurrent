1dcd2e997af4d49a8a315008fee3eb679578ea94
Rendall Schijven <rendall.schijven@hotmail.com>
2018-02-19 15:45:35 +0100

    Added dotproduct

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -48,5 +48,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/Dotproduct.dir/DependInfo.cmake"
   "CMakeFiles/Mandelbrot.dir/DependInfo.cmake"
   )
